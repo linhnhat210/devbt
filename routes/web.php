023e9c26ebd74d1197e2689c7a05d93bfce37aa4
Nhật Linh <147930044+linhnhat210@users.noreply.github.com>
2025-06-24 20:46:53 +0700

    test deploy after clone repo

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\WarehouseController;
 use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
-    return "hello word";
+    return "Xin chao the gioi";
 })->name('home');
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login')->middleware('guest');
