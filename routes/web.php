299351994b0390ca63575d6b1eefe245b8260f74
Nhật Linh <147930044+linhnhat210@users.noreply.github.com>
2025-06-24 21:18:49 +0700

    test deploy after add pub key

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\WarehouseController;
 use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
-    return "Xin chào thế giớiiiiiiiiiiiiiii1";
+    return "Xin chào thế giớiiiiiiiiiiiiiii123";
 })->name('home');
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login')->middleware('guest');
