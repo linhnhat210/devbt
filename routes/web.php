c4a2d8a32377c729e4b0699a7c5feb826a442363
Nhật Linh <147930044+linhnhat210@users.noreply.github.com>
2025-06-24 23:01:39 +0700

    test deploy after clean authorized_keys

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\WarehouseController;
 use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
-    return "Xinnn la xinn";
+    return "Xinnn la xinn123";
 })->name('home');
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login')->middleware('guest');
