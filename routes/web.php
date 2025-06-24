30800dc564f1ff6412a24eea39f5f022a08a304d
Nhật Linh <147930044+linhnhat210@users.noreply.github.com>
2025-06-24 20:36:44 +0700

    update home page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\WarehouseController;
 use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
-    return view('client.home');
+    return "hello";
 })->name('home');
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login')->middleware('guest');
