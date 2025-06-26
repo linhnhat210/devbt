3dfabd5f006d1bb636eaf8da9010913a13c365e7
Nhật Linh <147930044+linhnhat210@users.noreply.github.com>
2025-06-26 20:55:17 +0700

    test deploy with updated key

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\WarehouseController;
 use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
-    return "Xinnn la xasdeiann1234";
+    return "Xinnn la xasdedfiann1234";
 })->name('home');
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login')->middleware('guest');
